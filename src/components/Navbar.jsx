c32ea4c94588a816fda711692b735a58fe490616
MeryAmun <chebesiawah@gmail.com>
2022-11-04 12:47:39 +0100

    sparkline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,16 +20,33 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
+      />
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 );
 //2348067557080
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked,handleClick } =
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked,handleClick,screenSize, setScreenSize } =
     useStateContext();
+
+
+    useEffect(() => {
+     const handleResize = () => setScreenSize(window.innerWidth);
+     window.addEventListener('resize', handleResize);
+     handleResize()
+     window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+     if(screenSize <= 900){
+      setActiveMenu(false)
+     }else{
+      setActiveMenu(true)
+     }
+    }, [screenSize])
+    
+    
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
  import { useStateContext } from '../context/ContextProvider.js';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+  if(activeMenu && screenSize <= 900){
+    setActiveMenu(false)
+  }
+  }
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
   return (
@@ -16,7 +22,7 @@ const Sidebar = () => {
       activeMenu && (<>
       <div className='flex justify-between items-center'>
         <Link to='/' className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900' 
-        onClick={() => setActiveMenu(false)}>
+        onClick={handleCloseSidebar}>
           <SiShopware/><span>
           Shoppy
           </span>
@@ -45,7 +51,7 @@ className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
           <NavLink 
           to={`/${link.name}`}
           key ={link.name}
-          onClick={ () => {}}
+          onClick={handleCloseSidebar}
           className={({isActive}) => isActive ? activeLink : normalLink }
           >
             {link.icon}
